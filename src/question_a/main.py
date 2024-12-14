611aecda0193dfd8cd71da827e4e80627e84857b
Elena-Martinez-cloud <elena.martinez-elejarza@g.austincc.edu>
2024-12-14 05:21:21 +0000

    question a

--- src/question_a/main.py
+++ src/question_a/main.py
@@ -1,1 +1,18 @@
-#add import
+
+from question_a import create_multiplication_table
+from question_a import display_multiplication_table
+
+
+def main ():
+    while True:
+        multiplication_table = create_multiplication_table()
+
+        display_multiplication_table(multiplication_table)
+
+        user_input = input("Would you like to continue? (y/n): ").strip().lower()
+
+        if user_input == 'n':
+            print("Exiting Now")
+            break
+
+main()
--- src/question_a/question_a.py
+++ src/question_a/question_a.py
@@ -1,3 +1,19 @@
 #write functions here, don't add input('') statements here!
 def test_config():
     return True
+
+#-Create a create_mulitiplication_table function that returns a list.
+def create_multiplication_table():
+    table = []
+
+    for i in range(1, 6):
+        row = []
+        for j in range(1, 11):
+            row.append(i*j)
+        table.append(row)
+    return table
+
+#Create a display_multiplication_table that displays a table and accepts a list parameter
+def display_multiplication_table(table):
+    for row in table:
+        print(" ".join(str(value) for value in row))
