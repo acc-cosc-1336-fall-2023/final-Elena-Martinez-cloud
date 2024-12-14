d385b6b5978e627e45c76f46f5216f3831304ad3
Elena-Martinez-cloud <elena.martinez-elejarza@g.austincc.edu>
2024-12-14 06:07:46 +0000

    question b

--- src/question_b/main.py
+++ src/question_b/main.py
@@ -1,1 +1,29 @@
-#add import
+from question_b import display_stock_history, get_stock_list
+
+#The program continues until the user opts out. Create a main function, 
+#Call the get_stock_list and save the return value to a variable.
+#Create the following menu:
+#     1-Display stock purchase history
+#     2-Exit
+#     Option 1 calls the stock_list function and displays the data from the list.
+#     Option 2 exits the program#
+
+def main():
+    stock_list = get_stock_list()
+
+    while True:
+        print("\Menu:")
+        print("1 - Display stock purchase history")
+        print("2 - Exit")
+
+        choice = input("Please choose 1 or 2: ")
+
+        if choice == "1":
+            display_stock_history(stock_list)
+        elif choice == "2":
+            print("Exiting the program.")
+            break
+        else:
+            print("Invalid! Please choose from 1 or 2.")
+
+main ()
--- src/question_b/question_b.py
+++ src/question_b/question_b.py
@@ -1,1 +1,33 @@
-#write functions here, don't add input('') statements here!
+#Create a Stock class with one constructor with symbol and company_name parameters, 
+#private attributes(variables) symbol and company_name, and public functions get_symbol and 
+#get_company_name.
+
+class Stock:
+    def __init__(self, symbol, company_name):
+        
+        self.__symbol = symbol
+        self.__company_name = company_name
+
+    def get_symbol(self):
+        return self.__symbol
+    
+    def get_company_name(self):
+        return self.__company_name
+    
+
+#Create a function get_stock_list - create 5 Stock class instance variables/objects
+def get_stock_list():
+    stock1 = Stock("APPL", "Apple Inc.")
+    stock2 = Stock("CAT", "Caterpillar")
+    stock3 = Stock("EK", "Eastman Kodak")
+    stock4 = Stock("GOOG", "Google")
+    stock5 = Stock("MSFT", "Microsoft")
+
+
+#Add them to a list, return the list.
+    stock_list = [stock1, stock2, stock3, stock4, stock5]
+    return stock_list
+
+def display_stock_history(stock_list):
+    for stock in stock_list:
+        print(f"Symbol: {stock.get_symbol()}   Company: {stock.get_company_name()}")
